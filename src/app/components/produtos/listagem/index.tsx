0d3a648081678e20f328183a91ebee58e60f1a7a
José Vitor <jose.vitorw018@gmail.com>
2025-11-28 23:25:47 -0300

    Feito a tabela de listar os produtos

--- src/app/components/produtos/listagem/index.tsx
+++ src/app/components/produtos/listagem/index.tsx
@@ -1,13 +1,20 @@
 import { Layout } from "app/components/layout"
 import Link from "next/link"
-
+import { TabelaProdutos } from "./tabela"
+import {Produto} from 'app/app/models/produtos'
 export const ListagemProdutos: React.FC = () => {
+
+    const produtos: Produto[] = [{
+        id: "1", sku: "3232", nome: "Teste", preco: 234
+    }]
+
     return(
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
             <button className="button is-warning">Novo</button>
             </Link>
             <br />
+            <TabelaProdutos produtos={produtos}/>
         </Layout>
     )
 }
---
+++ src/app/components/produtos/listagem/tabela/index.tsx
@@ -0,0 +1,45 @@
+import { Produto} from 'app/app/models/produtos'
+
+interface TabelaProdutosProps{
+    produtos: Array<Produto>;
+}
+
+export const TabelaProdutos: React.FC<TabelaProdutosProps> = ({
+    produtos
+}) => {
+    return(
+        <table className="table is-hoverable is-striped">
+            <thead>
+                <th>Código</th>
+                <th>SKU</th>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th></th>
+            </thead>
+            <tbody>
+               {
+               produtos.map(produto => <ProdutoRow key={produto.id}produto={produto}/>)
+               }
+            </tbody>
+        </table>
+    )
+}
+interface ProdutoRowProps{
+    produto: Produto;
+}
+const ProdutoRow : React.FC<ProdutoRowProps> = ({
+    produto
+}) =>{
+    return(
+        <tr>
+            <td>{produto.id}</td>
+            <td>{produto.sku}</td>
+            <td>{produto.nome}</td>
+            <td>{produto.preco}</td>
+            <td>
+                <button className='button is-success'>Editar</button>
+                <button className='button is-danger'>Deletar</button>
+            </td>
+        </tr>
+    )
+}
