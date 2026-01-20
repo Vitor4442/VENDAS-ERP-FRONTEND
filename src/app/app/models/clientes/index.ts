15d15f4df7592adddeab9e70dc2c361fcf94ac49
José Vitor <jose.vitorw018@gmail.com>
2026-01-19 22:29:42 -0300

    Feito o model de clientes

---
+++ src/app/app/models/clientes/index.ts
@@ -0,0 +1,10 @@
+export interface Cliente {
+    id?: number;
+    nome?: string;
+    cpf?: string;
+    dataNascimento?: string;
+    endereco?: string;
+    email?: string;
+    telefone?: string;
+    dataCadastro?: string;
+}
